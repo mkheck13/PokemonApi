50de5a4dfa871c7bf1f9deaee6e52343428750e4
mkheck13 <mkheck13@gmail.com>
2024-01-31 20:41:36 -0800

    working on getting evos to work

--- scripts/app.js
+++ scripts/app.js
@@ -5,25 +5,21 @@ let favBtn = document.getElementById("favBtn");
 let pokeSearch = document.getElementById("pokeSearch");
 let searchBtn = document.getElementById("searchBtn");
 let pokeImg = document.getElementById("pokeImg");
-// let pokeEvo = document.getElementById("pokeEvo");
-// let pokeInfo = document.getElementById("pokeInfo");
-// let pokeType = document.getElementById("pokeType");
-// let pokeLocal = document.getElementById("pokeLocal");
-// let pokeAblity
-// let pokeBio = document.getElementById("pokeBio");
-// let pokeMoves = document.getElementById("pokeMoves");
-// let pokeStats = document.getElementById("pokeStats");
-let data = "";
-// let pokemon = "";
+let pokeEvo = document.getElementById("pokeEvo");
+let data;
+
+// Load on refresh and page load
+document.addEventListener('DOMContentLoaded', function() {
+    pokemonApi(1);
+});
 
 // API Call
-
 const pokemonApi = async (pokemon) => {
     pokeSearch.value = "";
     const promise = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     const data = await promise.json();
-    // console.log(data);
-    // return data;
+
+
 
     // Image
     pokeImg.src = data.sprites.other["official-artwork"].front_default;
@@ -33,21 +29,54 @@ const pokemonApi = async (pokemon) => {
     pokeMoves.innerText = "Moves: " + moveArray.join(", ");
 
     // Stats
+    pokeHp.textContent = `HP- ${data.stats[0].base_stat}`;
+    pokeAtk.textContent = `ATK- ${data.stats[1].base_stat}`;
+    pokeDef.textContent = `DEF- ${data.stats[2].base_stat}`;
+    pokeSpAtk.textContent = `SP. ATK- ${data.stats[3].base_stat}`;
+    pokeSpDef.textContent = `SP. DEF- ${data.stats[4].base_stat}`;
+    pokeSpd.textContent = `SPD- ${data.stats[5].base_stat}`;
+    
 
 
     // Evolution
+
+    // const evoGet = await fetch(`${data.species.url}`);
+    // const evoData = await evoGet.json();
+
+    // const evoChain = await fetch(`${evoData.evolution_chain.url}`);
+    // const evoChainData = await evoChain.json();
+
+    // if(evoChainData.chain.evoles_to.length === 0){
+    //     pokeEvo.textContent = "N/A";
+    // }else{
+    //     const evoArr = [evoChainData.chain.species.name];
+
+    //     const seeEvos = chain => {
+    //         if (chain.evoles_to.length === 0){
+    //             return;
+    //         }else{
+    //             chain.evoles_to.forEach(evo => {
+    //                 evoArr.push(evo.species.name);
+    //                 seeEvos(evo);
+    //             });
+    //         }
+    //     };
+    //     seeEvos(evoChainData.chain);
+    //     pokeEvo.textContent = evoArr.join(" > ");
+    // }
+
 
 
     // Name / Number
     pokeInfo.innerText = data.name;
 
+    pokeNum.innerText = data.id.toString().padStart(3, '0');
 
     // Type
     const typeArray = data.types.map(type => type.type.name);
     pokeType.innerText = "Type: " + typeArray.join(", ");
 
     // Location
-
     const locationPromise = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/encounters`);
     const location = await locationPromise.json();
         if(location.length > 0){
@@ -71,18 +100,23 @@ const pokemonApi = async (pokemon) => {
 
 // Search button and random button
 
-searchBtn.addEventListener('click', () => {
-    pokeSearch.value ?pokemonApi(pokeSearch.value): alert("That's not a vaild Pokemon name.")
+searchBtn.addEventListener('click', async () => {
+    if( pokeSearch.value){
+        currentPokemon = await pokemonApi(pokeSearch.value.toLowerCase());
+    }
 });
 
-// pokeSearch.addEventListener('keypress', (e) => {
-//     if(e.key === 'Enter'){
-//         pokeType.innerHTML = "";
-//         pokemonApi(pokeSearch.value.toLowerCase());
-//     }
-// });
+pokeSearch.addEventListener('keydown', async (event) => {
+    if (pokeSearch.value) {
+        if (event.key === 'Enter') {
+            currentPokemon = await pokemonApi(event.target.value.toLowerCase());
+        }
+    }
+});
 
-randBtn.addEventListener("click", () => {
+
+
+randBtn.addEventListener("click", async () => {
     let random = Math.floor(Math.random() * 649) + 1;
     pokemonApi(random);
   });
