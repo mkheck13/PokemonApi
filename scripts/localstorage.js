d60df48d8973c552974fc247d71a0941ee9e51b7
mkheck13 <mkheck13@gmail.com>
2024-02-02 15:53:39 -0800

    flowbite push

--- scripts/localstorage.js
+++ scripts/localstorage.js
@@ -1,44 +1,22 @@
 const saveToLocalStorage = (pokemon) =>{
-
     let favorites = getlocalStorage();
-
-
     if(!favorites.includes(pokemon)){
         favorites.push(pokemon);
     }
-
-
     localStorage.setItem("Favorites", JSON.stringify(favorites));
 }
-
 const getlocalStorage = () => {
-
-
     let localStorageData = localStorage.getItem("Favorites");
-
-
     if(localStorageData === null){
         return [];
     }
-
-
     return JSON.parse(localStorageData);
-
 }
-
 const removeFromLocalStorage = (pokemon) =>{
-
-
     let favorites = getlocalStorage();
-
-
     let namedIndex = favorites.indexOf(pokemon);
-
     favorites.splice(namedIndex, 1);
-    
-
-    localStorage.setItem("favorites", JSON.stringify(favorites));
-    
+    localStorage.setItem("favorites", JSON.stringify(favorites)); 
 }
 
 export {saveToLocalStorage, getlocalStorage, removeFromLocalStorage};
